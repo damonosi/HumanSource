299f5ae6e1a64b5f76ec23035fa4a995fa0077cf
andrei <dam195@yahoo.com>
2023-10-31 18:05:59 +0200

    fish

--- next.config.js
+++ next.config.js
@@ -7,16 +7,14 @@ const nextConfig = {
 	async headers() {
 		return [
 			{
-				// Routes this applies to
 				source: "/api/(.*)",
-				// Headers
+
 				headers: [
-					// Allow for specific domains to have access or * for all
 					{
 						key: "Access-Control-Allow-Origin",
-						value: process.env.ALLOWED_ORIGIN,
+						value: "backend.humansource.ro",
 					},
-					// Allows for specific methods accepted
+
 					{
 						key: "Access-Control-Allow-Methods",
 						value: "GET, POST, PUT, DELETE, OPTIONS",
