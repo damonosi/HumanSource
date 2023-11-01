6dea7bca34c7eda5ddecc4bb1d03ce688bcd6c0f
andrei <dam195@yahoo.com>
2023-11-01 14:34:47 +0200

    job application

--- src/lib/apollo/mutations/mutateJobAplication.ts
+++ src/lib/apollo/mutations/mutateJobAplication.ts
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 const AddJobApplication = gql`
-	mutation Mutation($data: JobApplicationCreateInput!!) {
+	mutation Mutation($data: JobApplicationCreateInput!) {
 		createJobApplication(data: $data) {
 			email
 		}
