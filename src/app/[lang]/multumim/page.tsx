6e28fcf79ab56fceabb39c37d5ce3f89ded1cb1a
andrei <dam195@yahoo.com>
2023-11-08 12:05:11 +0200

    style multumim

--- src/app/[lang]/multumim/page.tsx
+++ src/app/[lang]/multumim/page.tsx
@@ -1,13 +1,18 @@
 const MultumimPage = () => {
 	return (
-		<section className="flex min-h-screen items-center  justify-center">
-			<div className=" flex min-h-screen max-w-sm flex-col items-center justify-center gap-7 text-center">
-				<h1 className="text-5xl font-[700]">Multumim!</h1>
-				<h2 className="text-2xl font-[500]">Ai făcut primul pas spre o carieră ca șofer profesionist în Italia!</h2>
-				<h3 className="text-2xl font-[500]">
-					Unul dintre agenții noștri de recrutare te va contacta în cel mai scurt timp posibil.ră ca șofer profesionist
-					în Italia!
+		<section className="flex  items-center  justify-center">
+			<div className=" flex max-w-sm flex-col items-center justify-center gap-7 px-2 py-24 text-center">
+				<h1 className="text-4xl font-[700] md:text-5xl">Multumim!</h1>
+				<h2 className="text-base font-[500] md:text-2xl">
+					Ai făcut primul pas spre o carieră ca șofer profesionist în Italia!
+				</h2>
+				<h3 className="text-base font-[500] md:text-2xl">
+					Unul dintre agenții noștri de recrutare te va contacta în cel mai scurt timp posibil ca șofer profesionist în
+					Italia!
 				</h3>
+				<button className="flex items-center justify-center gap-3 rounded-2xl bg-gri-brand py-4 px-5 text-alb-site">
+					Intreaba-ne ceva
+				</button>
 			</div>
 		</section>
 	);
