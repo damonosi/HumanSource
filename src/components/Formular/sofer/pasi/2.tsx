5fbc15471ecc6dd67f7a2ced10453e60874dd0d9
andrei <dam195@yahoo.com>
2023-11-13 11:06:15 +0200

    ring remove

--- src/components/Formular/sofer/pasi/2.tsx
+++ src/components/Formular/sofer/pasi/2.tsx
@@ -35,7 +35,7 @@ const Checkbox = ({
 
 						setDisabled(false);
 					}}
-					className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
+					className="h-4 w-4  rounded-full border-gray-300 bg-gray-100 text-blue-600   focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
 				/>
 
 				<span className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"> {varianta} </span>
