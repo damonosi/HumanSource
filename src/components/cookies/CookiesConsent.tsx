e177f1eaf89cff8809db8d5ed80ad868a4075bcf
andrei <dam195@yahoo.com>
2023-11-15 19:41:15 +0200

    cookies settings

--- src/components/cookies/CookiesConsent.tsx
+++ src/components/cookies/CookiesConsent.tsx
@@ -1,13 +1,16 @@
 "use client";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import { useTranslation } from "@/app/i18n/client";
 import { useCookies } from "next-client-cookies";
-
+import { Switch } from "@material-tailwind/react";
+import { IoMdArrowRoundBack } from "react-icons/io";
 const CookieConsent = ({ params }: { params: { lang: string } }) => {
 	const [showConsent, setShowConsent] = useState(true);
+	const [checked, setChecked] = useState(true);
 	const [showSettingsPage, setShowSettingsPage] = useState(false);
 	const cookies = useCookies();
+
 	React.useEffect(() => {
 		cookies.get("localConsent") === undefined && setShowConsent(false);
 	}, []);
@@ -25,14 +28,41 @@ const CookieConsent = ({ params }: { params: { lang: string } }) => {
 	if (showConsent) {
 		return null;
 	}
+
 	return (
 		<div className="fixed z-50 -mt-8  flex h-full w-full items-center justify-center bg-opacity-70 px-2 backdrop-blur-sm  md:px-20">
-			<div className="   flex max-w-3xl flex-col items-center justify-between gap-16 rounded-2xl bg-alb-site p-8 md:p-16">
+			<div className=" relative  flex max-w-3xl flex-col items-center justify-between gap-16 rounded-2xl bg-alb-site p-8 md:p-16">
 				{" "}
 				{showSettingsPage ? (
-					<div>
-						<button onClick={() => setShowSettingsPage(false)}>back</button>
-						<span>settings</span>
+					<div className=" flex flex-col gap-6 overflow-x-hidden overflow-y-scroll px-2">
+						<IoMdArrowRoundBack
+							className="absolute top-4 left-4 h-5 w-5 cursor-pointer hover:scale-105"
+							onClick={() => setShowSettingsPage(false)}
+						/>
+						<div>
+							<div className="my-6 flex items-center justify-between">
+								<span className="font-bold"> {t("settingsPage.switch1.denumire")}</span>
+								<Switch id="custom-switch-component1" disabled={true} defaultChecked />
+							</div>
+							<span>{t("settingsPage.switch1.descriere")}</span>
+						</div>
+						<div>
+							<div className="my-6 flex items-center justify-between">
+								<span>{t("settingsPage.switch2.denumire")}</span>
+								<Switch
+									id="custom-switch-component2"
+									value={"checked"}
+									onChange={() => {
+										setChecked((prevCheck) => !prevCheck);
+										checked === true
+											? cookies.set("analytics", "true", { sameSite: "none" })
+											: cookies.set("analytics", "false", { sameSite: "none" });
+										console.log(cookies.get("analytics"));
+									}}
+								/>
+							</div>
+							<span>{t("settingsPage.switch2.descriere")}</span>{" "}
+						</div>
 					</div>
 				) : (
 					<div>
