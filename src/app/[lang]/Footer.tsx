6ae566b1793b1d1c2f568d7a484793fd8cc55931
andrei <dam195@yahoo.com>
2023-11-15 07:58:50 +0200

    repair

--- src/app/[lang]/Footer.tsx
+++ src/app/[lang]/Footer.tsx
@@ -8,7 +8,7 @@ import { SlSocialTwitter } from "react-icons/sl";
 import { useTranslation } from "../i18n/client";
 
 const FooterNavItem = ({ params, href, label }: { params: { lang: string }; href: string; label: string }) => (
-	<Link className=" flex  items-start" href={`/${params.lang}/${href}`}>
+	<Link className=" flex items-center md:items-start" href={`/${params.lang}/${href}`}>
 		<Typography
 			variant="small"
 			className={` relative flex  items-center  p-1 font-[350] text-alb-site    before:absolute before:-bottom-1 before:left-0 before:block before:h-[2px] 
@@ -25,14 +25,14 @@ const Footer = ({ params }: { params: { lang: string } }) => {
 	const { t } = useTranslation(params.lang, "footer");
 
 	return (
-		<footer className="relative z-50  w-full overflow-hidden bg-gri-bg pt-12 pb-10 text-white md:mx-auto   md:items-center  md:justify-center">
+		<footer className="relative z-40  w-full overflow-hidden bg-gri-bg pt-12 pb-10 text-white md:mx-auto   md:items-center  md:justify-center">
 			<div className="mx-auto  flex w-full flex-col items-center justify-center  pb-0   md:px-[70px]">
 				<nav className="container z-30 flex w-full flex-col  items-center justify-between  md:items-start  ">
 					<div className=" flex w-full  flex-col items-center justify-center gap-8 gap-y-6 p-4 md:items-start  md:justify-between lg:flex-row ">
 						<div className="flex w-full flex-col items-center justify-center gap-6 text-center md:w-1/4 md:items-start md:text-start ">
 							<h6 className=" text-[16px]  font-bold leading-[20px]">HUMAN SOURCE</h6>
 							<hr className="h-2 w-full" />
-							<div className="flex flex-col gap-8 text-[16px]  font-[350] leading-[24px]">
+							<div className="flex flex-col items-center gap-8 text-[16px] font-[350]  leading-[24px] md:items-start">
 								<FooterNavItem params={params} href={`about`} label={t("1.despre")} />
 								<FooterNavItem params={params} href={`about`} label={t("1.angajatori")} />
 								<FooterNavItem params={params} href={`blog`} label={t("1.candidati")} />
@@ -43,7 +43,7 @@ const Footer = ({ params }: { params: { lang: string } }) => {
 							<h6 className="font-bold">{t("2.titlu")}</h6>
 
 							<hr className="h-2 w-full" />
-							<div className="flex flex-col gap-8">
+							<div className="flex flex-col items-center  gap-8 md:items-start">
 								<FooterNavItem params={params} href={`services/#sectiune-servicii-1`} label={t("2.leasing")} />
 								<FooterNavItem params={params} href={`services/#sectiune-servicii-2`} label={t("2.recrutare")} />
 								<FooterNavItem params={params} href={`services/#sectiune-servicii-3`} label={t("2.payroll")} />
@@ -61,7 +61,7 @@ const Footer = ({ params }: { params: { lang: string } }) => {
 						<div className="flex w-full flex-col items-center justify-center gap-6 text-center md:w-1/4 md:items-start md:text-start">
 							<h6 className="font-bold">{t("4.titlu")}</h6>
 							<hr className="h-2 w-full" />
-							<div className="flex flex-col gap-8">
+							<div className="flex flex-col items-center gap-8 md:items-start">
 								<FooterNavItem params={params} href={`about`} label={t("4.politica")} />
 								<FooterNavItem params={params} href={`blog`} label={t("4.termeni")} />
 								<FooterNavItem params={params} href={`contact`} label={t("4.cookies")} />
