0164a63533a952a99e2842bf867b8c406d683587
andrei <dam195@yahoo.com>
2023-11-15 13:33:25 +0200

    applelul pulii

--- src/app/[lang]/error.tsx
+++ src/app/[lang]/error.tsx
@@ -15,13 +15,15 @@ export default function Error({ error, reset }: { error: Error; reset: () => voi
 	const params = useParams();
 	const language: lang = params.lang as string;
 
-	const { t } = useTranslation(language, "error");
+
 	return (
 		<div className="flex min-h-screen flex-col md:flex-row">
 			<div className="mt-5 flex flex-col items-center justify-center gap-7 px-5 md:w-1/2">
 				<h1 className="text-[2rem] font-[800]">Ooops.. </h1>
-				<h2 className="text-xl font-bold">{t("mesaj1")}</h2>
-				<h3 className="text-sm font-[350]">{t("mesaj2")}</h3>
+				<h2 className="text-xl font-bold">Pagina nu functioneaza momentan</h2>
+				<h3 className="text-sm font-[350]">
+					Lorem ipsum dolor sit amet consectetur. In enim semper leo quam pharetra lectus diam.{" "}
+				</h3>
 				<button
 					className="flex items-center justify-center gap-3 rounded-2xl bg-gri-brand py-4 px-5 text-alb-site"
 					onClick={() => {
@@ -31,7 +33,7 @@ export default function Error({ error, reset }: { error: Error; reset: () => voi
 					<span className="flex items-center justify-center">
 						<HomeIco />
 					</span>{" "}
-					{t("buton")}
+					Inapoi acasa
 				</button>
 			</div>
 			<div className="flex flex-col items-center justify-center md:w-1/2  md:items-start">
