63ce5fb04c784e1d5ed3c54d6b437b457f22e1a2
Marius <garai.marius@gmail.com>
2023-11-27 20:31:59 +0200

    modify with env variable for ulr

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-	siteUrl: process.env.SITE_URL || "https://humansource.vercel.app",
+	siteUrl: process.env.SITE_URL || `${process.env.NEXT_PUBLIC_SITE_URL}`,
 	generateRobotsTxt: true, // (optional)
 	// ...other options
 };
--- src/app/[lang]/sitemap.ts
+++ src/app/[lang]/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
 	return [
 		{
-			url: "https://humansource.vercel.app",
+			url: `${process.env.NEXT_PUBLIC_SITE_URL}`,
 			lastModified: new Date(),
 			changeFrequency: "yearly",
 			priority: 1,
