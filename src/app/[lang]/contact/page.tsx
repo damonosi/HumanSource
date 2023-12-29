a26ec64c2b76de55f89ca0d95965dc4e13175263
andrei <dam195@yahoo.com>
2023-12-29 13:12:01 +0200

    reparat greseala metadata contact page

--- src/app/[lang]/contact/page.tsx
+++ src/app/[lang]/contact/page.tsx
@@ -1,11 +1,9 @@
 import { Metadata } from "next";
 import ClientContactPage from "./ClientContactPage";
 export const metadata: Metadata = {
-	title: "Formular de cerere oferta pentru companii - Humansource.ro ",
-	description:
-		"Ai nevoie de personal pentru desfasurarea activitatii tale? Completeaza formularul si cere o oferta pentru a te putea ajuta! ✔️ Flexibilitate ✔️ Transparenta",
+	title: "Contact - Human Source",
+	description: "Human Source Contact Page",
 };
-
 const Contact = ({ params }: { params: { lang: string; country: string } }) => {
 	return (
 		<section className="  bg-[#E5E5E5] px-4 pb-[100px] md:px-[70px]">
